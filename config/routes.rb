61f1a9df24a7ec5932413e23c8c5ab77a6ffe587
Jim Nanney <jnanney@gmail.com>
2013-11-01 23:30:39 -0500

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rubyfriends::Application.routes.draw do
 
   get '/about', to: 'tweets#about', as: :about
+  get '/home', to: 'tweets#home', as: :home
   resources :tweets, only: [:index, :show]
-  root to: 'tweets#index'
+  root to: 'tweets#home'
 
 end
